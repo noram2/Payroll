28fcb942e3a88adf2fc3be3e897106815a9cf2a0
noram2 <norairismitchell@yahoo.com>
2014-08-15 20:08:50 -0400

    formatting fixes

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -1,16 +1,26 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by noramitchell on 8/14/14.
  */
+@SuppressWarnings("ALL")
 public class Employee {
     private static double MinimumWage = 7.50;
+    private static double MaximumHours = 60;
+    private static Set<Integer> idList = new HashSet<Integer>();
     private int id;
     private String name;
     private double hourlyRate;
     private double hoursWorked;
 
     public Employee(int id, String name) {
+        if (idList.contains(id))
+            throw new IllegalArgumentException("employee id must be unique " + id);
+        idList.add(id);
+
         this.id = id;
         this.name = name;
     }
@@ -46,6 +56,8 @@ public class Employee {
     }
 
     public void setHoursWorked(double hoursWorked) {
+        if (hoursWorked > MaximumHours)
+            throw new IllegalArgumentException("number of hours must be less than " + MaximumHours);
         this.hoursWorked = hoursWorked;
     }
 
